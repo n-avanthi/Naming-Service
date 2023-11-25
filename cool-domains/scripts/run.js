9c01f242870319fe535a49584ab53037424cd06b
Avanthi Narasingu <narasinguavanthi@gmail.com>
2023-11-25 23:53:01 +0530

    updating domains.sol & run.js
    Set up domains, resolvers and records and updating domains.sol & run.js

--- cool-domains/scripts/run.js
+++ cool-domains/scripts/run.js
@@ -1,21 +1,21 @@
-
 const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
-    //await domainContract.deployed();
-    console.log("Contract deployed to:", await domainContract.getAddress());
- // };
     //await domainContract.deployed(); no need to used .deployed()
     console.log("Contract deployed to:", await domainContract.getAddress()); // use this .getAddress()
     console.log("Contract deployed by:", owner.address);
-
+    
     const txn = await domainContract.register("doom");
     await txn.wait();
 
     const domainOwner = await domainContract.getAddress("doom");
     console.log("Owner of domain:", domainOwner);
-  };
+
+    // Trying to set a record that doesn't belong to me!
+    txn = await domainContract.connect(randomPerson).setRecord("doom", "Haha my domain now!");
+    await txn.wait();
+  }
 
   const runMain = async () => {
     try {
