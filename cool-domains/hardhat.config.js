c869d5d9c869aa6417a0dd8d350102be8711c8a0
Avanthi Narasingu <narasinguavanthi@gmail.com>
2023-11-26 21:26:46 +0530

    updating deploy.js
    deploying smart contract

--- cool-domains/hardhat.config.js
+++ cool-domains/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    mumbai: {
+      url: "https://delicate-morning-leaf.matic-testnet.quiknode.pro/f8a070a7298172b259e221f23056e584368baf53/",
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    }
+  }
 };
